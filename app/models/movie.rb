708e921053f93cdf3ef9812c3941e2bce6691c23
Roberto Baladrón Pardo <rbaladron@gmail.com>
2018-08-24 11:51:59 +0000

    Solucionados problemas con seed.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
-     attr_accessor :title, :rating, :description, :release_date
+     #attr_accessible :title, :rating, :description, :release_date ==> No se usa en Rails 5
+     
+  def self.ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
 end
